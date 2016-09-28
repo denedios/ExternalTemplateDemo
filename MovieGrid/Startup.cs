4d915ee97afa0822866f4992b8cda226a17af182
Chris Denedios <chris.denedios@therapeuticsmd.com>
2016-09-28 14:25:37 -0400

    initial project and solution creation

---
+++ MovieGrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieGrid.Startup))]
+namespace MovieGrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
