a73de525af7b2594273228750196e6bc1660fa95
Chris Denedios <denedios@gmail.com>
2016-09-30 15:46:28 -0400

    extended Movie model to allow for details modal modified GetAllMovies to convert json to DataSourceResult for grid

--- MovieGrid/Controllers/HomeController.cs
+++ MovieGrid/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
 using MovieGrid.Models;
 
 namespace MovieGrid.Controllers
@@ -12,7 +14,7 @@ namespace MovieGrid.Controllers
             return View();
         }
 
-        public ActionResult GetAllMovies()
+        public ActionResult GetAllMovies([DataSourceRequest]DataSourceRequest request)
         {
             var movies = new List<Movie>
             {
@@ -21,39 +23,59 @@ namespace MovieGrid.Controllers
                     MovieId = 1,
                     Title = "Miss Peregrine's Home for Peculiar Children",
                     Description = "When Jacob discovers clues to a mystery that stretches across time, he finds Miss Peregrine's Home for Peculiar Children. But the danger deepens after he gets to know the residents and learns about their special powers.",
-                    ReleaseDate = new DateTime(2016, 9, 30)
+                    ReleaseDate = new DateTime(2016, 9, 30),
+                    Rating = "PG-13",
+                    Director = "Tim Burton",
+                    Genre = "Adventure, Fantasy",
+                    Length = "2h 7min"
                 },
                 new Movie
                 {
                     MovieId = 2,
                     Title = "The Shawshank Redemption",
                     Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-                    ReleaseDate = new DateTime(1994, 10, 14)
+                    ReleaseDate = new DateTime(1994, 10, 14),
+                    Rating = "R",
+                    Director = "Frank Darabont",
+                    Genre = "Crime, Drama",
+                    Length = "2h 22min"
                 },
                 new Movie
                 {
                     MovieId = 3,
                     Title = "The Godfather",
                     Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-                    ReleaseDate = new DateTime(1972, 3, 24)
+                    ReleaseDate = new DateTime(1972, 3, 24),
+                    Rating = "R",
+                    Director = "Francis Ford Coppola",
+                    Genre = "Crime, Drama",
+                    Length = "2h 55min"
                 },
                 new Movie
                 {
                     MovieId = 4,
                     Title = "The Dark Knight",
                     Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
-                    ReleaseDate = new DateTime(2008, 7, 18)
+                    ReleaseDate = new DateTime(2008, 7, 18),
+                    Rating = "PG-13",
+                    Director = "Christopher Nolan",
+                    Genre = "Action, Crime, Drama",
+                    Length = "2h 32min"
                 },
                 new Movie
                 {
-                    MovieId = 1,
+                    MovieId = 5,
                     Title = "Pulp Fiction",
                     Description = "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-                    ReleaseDate = new DateTime(1994, 10, 14)
+                    ReleaseDate = new DateTime(1994, 10, 14),
+                    Rating = "R",
+                    Director = "Quentin Tarantino",
+                    Genre = "Crime, Drama",
+                    Length = "2h 34min"
                 }
             };
 
-            return Json(movies, JsonRequestBehavior.AllowGet);
+            return Json(movies.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
         }
     }
 }
--- MovieGrid/Models/Movie.cs
+++ MovieGrid/Models/Movie.cs
@@ -11,6 +11,10 @@ namespace MovieGrid.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public string Length { get; set; }
+        public string Genre { get; set; }
 
     }
 }
